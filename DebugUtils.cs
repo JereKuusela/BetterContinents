234a8e2248fd5d94c01c4231db84f0b1ac95c363
twilightsaurus <tuataur@googlemail.com>
2024-02-23 22:34:43 +0200

    Added Continent Size and Ocean Channels to debug options. (Allows to change these settings while in-game and see the result.

--- DebugUtils.cs
+++ DebugUtils.cs
@@ -163,11 +163,19 @@ public partial class DebugUtils
             bc.AddGroup("g", "Global", "global settings, get more info with 'bc g help'",
                 group =>
                 {
+                    group.AddValue("cs", "Continent size adjustment", "continent size adjustment",
+                        defaultValue: 0.5f, minValue: 0, maxValue: 1,
+                        setter: SetHeightmapValue<float>(value => BetterContinents.Settings.ContinentSize = value),
+                        getter: () => BetterContinents.Settings.ContinentSize);
                     group.AddValue("sl", "Sea level adjustment", "sea level adjustment",
                         defaultValue: 0.5f, minValue: 0, maxValue: 1,
                         setter: SetHeightmapValue<float>(value => BetterContinents.Settings.SeaLevel = value),
                         getter: () => BetterContinents.Settings.SeaLevel);
-                    group.AddValue("r", "Enable rivers", "whether rivers are enabled",
+                    group.AddValue("oc", "Ocean channels", "whether ocean channels are enabled",
+                        defaultValue: true,
+                        setter: SetHeightmapValue<bool>(value => BetterContinents.Settings.OceanChannelsEnabled = value),
+                        getter: () => BetterContinents.Settings.OceanChannelsEnabled);
+                    group.AddValue("r", "Rivers", "whether rivers are enabled",
                         defaultValue: true,
                         setter: SetHeightmapValue<bool>(value => BetterContinents.Settings.RiversEnabled = value),
                         getter: () => BetterContinents.Settings.RiversEnabled);
